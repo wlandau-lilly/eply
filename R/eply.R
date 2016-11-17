06aba84219e31f4c759e151bda34356995ac3ca7
Will Landau <will.landau@lilly.com>
2016-11-17 17:46:46 -0500

    edit eply fn

--- R/eply.R
+++ R/eply.R
@@ -9,20 +9,20 @@
 #' in each row of \code{.expr} are evaluated, and the results are given to 
 #' \code{.fun} as function arguments. The column names of \code{.expr} must
 #' contain the argument names of \code{.fun}.
-#' If \code{.tasks > 1} and \code{.split} is used, \code{parallel::mclapply}
-#' will be used to distribute the work over multiple parallel tasks, where tasks
-#' are split up according to the columns of \code{.expr} named in \code{.split}.
+#' With \code{.tasks} and \code{.split}, Mac and Linux users can 
+#' distribute the work over multple parallel tasks. See the vignette for an 
+#' example (\code{vignette("eply")}).
 #' @return a list or vector of return values of \code{.fun}.
 #' @param .fun function to evaluate.
 #' @param .expr data frame with function calls as rows and function arguments
 #' as columns. The elements are characters that encode unevaluated expressions.
 #' These expressions evaluate to function arguments of \code{.fun}. The argument
 #' names of \code{.fun} must be contained in the column names of \code{.expr}.
-#' @param .split character vector of columns of \code{.expr}
-#'  to split over when executing parallel jobs.
-#' @param .tasks number of parallel units of execution. Splits of \code{.expr}
-#' according to the variables in \code{.split} are distributed over \code{.tasks}
-#' parallel units of execution using \code{parallel::mclapply}. 
+#' @param .split character vector of columns of \code{.expr}, specifies how
+#' the work is distributed across parallel tasks. 
+#' See the vignette for an example \code{vignette("eply")}.
+#' @param .tasks number of parallel tasks for distributing the work.
+#' See the vignette for an example \code{vignette("eply")}.
 eply = function(.fun, .expr, .split = NULL, .tasks = 1){
   if(.tasks < 1 | !is.numeric(.tasks)) 
     stop("Argument .tasks must be a number at least 1.")
