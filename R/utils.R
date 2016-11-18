214ff38ff93a2999dec3fbf0580c816024656c8c
Will Landau <will.landau@lilly.com>
2016-11-18 08:30:15 -0500

    Add do_serial

--- R/utils.R
+++ R/utils.R
@@ -1,13 +1,39 @@
-#' @title Function \code{eval_text}
-#' @description Evaluate expressions encoded as
-#' elements of a character vector.
+#' @title Function \code{do_serial}
+#' @description Determine whether to execute \code{\link{eply}} serially.
 #' @seealso \code{\link{eply}}
 #' @export
-#' @return list of evaluated expressions
-#' @param x character vector encoding unevaluated expressions
-eval_text = Vectorize(FUN = function(x){
-  eval(parse(text = x))
-}, vectorize.arg = "x", SIMPLIFY = FALSE)
+#' @return \code{TRUE} if \code{\link{eply}} will run serially
+#' and \code{FALSE} if parallelized.
+#' @param .fun argument to \code{\link{eply}}
+#' @param .expr argument to \code{\link{eply}}
+#' @param .split argument to \code{\link{eply}}
+#' @param .tasks argument to \code{\link{eply}}
+#' @param os operating system
+do_serial = function(.fun, .expr, .split = NULL, .tasks = 1,
+  os = Sys.info()[['sysname']]){
+
+  # Sanitize individual arguments.
+  if(.tasks < 1 | !is.numeric(.tasks))
+    stop(".tasks must be a number at least 1.")
+  if(!all(.split %in% colnames(.expr)))
+    stop(".split must be a subset of colnames(.expr).")
+ 
+  # Resolve conflicts.
+  if(.tasks > 1 & !length(.split)){
+    warning(".tasks > 1 but .split is empty. Executing serially.")
+    .tasks = 1
+  } else if(.tasks <= 1 & length(.split) > 0){
+    warning(".tasks == 1 but .split is nonempty. Executing serially.")
+  } 
+
+  # Disable parallelism on Windows.
+  if(.tasks > 1 & os == "Windows"){
+    warning("Parallel execution not supported on Windows. Executing serially.")
+    .tasks = 1
+  } 
+
+  .tasks > 1
+}
 
 #' @title Function \code{eply_serial}
 #' @description Serial version of \code{\link{eply}}.
@@ -25,3 +51,14 @@ eply_serial = function(.fun, .expr){
   }) %>%
   unname
 }
+
+#' @title Function \code{eval_text}
+#' @description Evaluate expressions encoded as
+#' elements of a character vector.
+#' @seealso \code{\link{eply}}
+#' @export
+#' @return list of evaluated expressions
+#' @param x character vector encoding unevaluated expressions
+eval_text = Vectorize(FUN = function(x){
+  eval(parse(text = x))
+}, vectorize.arg = "x", SIMPLIFY = FALSE)
